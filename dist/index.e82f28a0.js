e64cc658b195a0b9916048be490ce7126cf8289c
keshavblack123 <keshavnatarajan@gmail.com>
2022-12-13 15:48:58 +0800

    Cube pictured material

--- dist/index.e82f28a0.js
+++ dist/index.e82f28a0.js
@@ -532,13 +532,19 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"dV6cC":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _three = require("three");
 var _datGui = require("dat.gui");
 var _orbitControlsJs = require("three/examples/jsm/controls/OrbitControls.js");
+var _starsJpg = require("../img/stars.jpg");
+var _starsJpgDefault = parcelHelpers.interopDefault(_starsJpg);
+var _nebulaJpg = require("../img/nebula.jpg");
+var _nebulaJpgDefault = parcelHelpers.interopDefault(_nebulaJpg);
 //Instantiate the renderer
 const renderer = new _three.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true;
 //Instantiate the scene and camera to be used in render method
 const scene = new _three.Scene();
 const camera = new _three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -551,31 +557,108 @@ camera.position.set(-10, 30, 30);
 orbit.update();
 //add a box in the scene
 const boxGeometry = new _three.BoxGeometry();
-const boxMaterial = new _three.MeshBasicMaterial({
+const boxMaterial = new _three.MeshStandardMaterial({
     color: 0xFF00FF
 });
 const box = new _three.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 //add a plane in the scene
 const planeGeometry = new _three.PlaneGeometry(30, 30);
-const planeMaterial = new _three.MeshBasicMaterial({
+const planeMaterial = new _three.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: _three.DoubleSide //so it doesnt dsappear when you go upside down
 });
 const plane = new _three.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI; //rotate plane inline with the grid
+plane.receiveShadow = true;
 // add grid, number controles the number of grids
 const gridHelper = new _three.GridHelper(30);
 scene.add(gridHelper);
 //add a sphere
 const sphereGeometry = new _three.SphereGeometry(4, 50, 50);
-const sphereMaterial = new _three.MeshBasicMaterial({
+const sphereMaterial = new _three.MeshStandardMaterial({
     color: 0x0000FF,
     wireframe: false
 });
 const sphere = new _three.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.castShadow = true;
+/*
+Ambeint light = light that comes from the environment (indirect result of other sources of light, daylight in your room)
+Directional light = (direct sunlight) big and far source of light that coveres the environment
+spotlight = light in the form of a cone, farther the source of the light the bigger the cone radius
+*/ //Add ambient light 
+const ambientLight = new _three.AmbientLight(0x333333);
+scene.add(ambientLight);
+/*
+//Add directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+scene.add(directionalLight);
+directionalLight.position.set( -30, 50, 0)
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.bottom = -12;
+
+//Directional Light helper
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(dLightHelper);
+
+//Shadow Camera
+const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(dLightShadowHelper);
+*/ //Spotlight
+const spotlight = new _three.SpotLight(0xffffff);
+scene.add(spotlight);
+spotlight.position.set(-100, 100, 0);
+spotlight.castShadow = true;
+spotlight.angle = 0.1;
+//Spotlight helper
+const sLightHelper = new _three.SpotLightHelper(spotlight);
+scene.add(sLightHelper);
+//Add fog to the scene
+// scene.fog = new THREE.Fog(0xffffff, 0, 200);
+scene.fog = new _three.FogExp2(0xffffff, 0.01);
+// renderer.setClearColor(0xffea00)
+const textureLoader = new _three.TextureLoader();
+// scene.background = textureLoader.load(nebula);
+const cubeTextureLoader = new _three.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    (0, _nebulaJpgDefault.default),
+    (0, _starsJpgDefault.default),
+    (0, _starsJpgDefault.default),
+    (0, _starsJpgDefault.default),
+    (0, _starsJpgDefault.default),
+    (0, _starsJpgDefault.default)
+]);
+const box2MultMaterial = [
+    new _three.MeshBasicMaterial({
+        map: textureLoader.load((0, _starsJpgDefault.default))
+    }),
+    new _three.MeshBasicMaterial({
+        map: textureLoader.load((0, _starsJpgDefault.default))
+    }),
+    new _three.MeshBasicMaterial({
+        map: textureLoader.load((0, _nebulaJpgDefault.default))
+    }),
+    new _three.MeshBasicMaterial({
+        map: textureLoader.load((0, _starsJpgDefault.default))
+    }),
+    new _three.MeshBasicMaterial({
+        map: textureLoader.load((0, _nebulaJpgDefault.default))
+    }),
+    new _three.MeshBasicMaterial({
+        map: textureLoader.load((0, _starsJpgDefault.default))
+    })
+];
+const box2Geometry = new _three.BoxGeometry(4, 4, 4);
+const box2Material = new _three.MeshBasicMaterial({
+    // color: 0x00FF00,
+    map: textureLoader.load((0, _nebulaJpgDefault.default))
+});
+const box2 = new _three.Mesh(box2Geometry, box2MultMaterial);
+scene.add(box2);
+box2.position.set(0, 15, 10);
+box2.name = "theBox";
 //reposition sphere
 sphere.position.set(-10, 10, 0);
 //allows user to control characteristics of sphere
@@ -583,7 +666,10 @@ const gui = new _datGui.GUI();
 const options = {
     sphereColor: "#ffea00",
     wireframe: false,
-    speed: 0.01
+    speed: 0.01,
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 1
 };
 gui.addColor(options, "sphereColor").onChange(function(e) {
     sphere.material.color.set(e);
@@ -593,18 +679,41 @@ gui.add(options, "wireframe").onChange(function(e) {
 });
 // add speed with upper and lower bounds
 gui.add(options, "speed", 0, 0.1);
+gui.add(options, "angle", 0, 1);
+gui.add(options, "penumbra", 0, 1);
+gui.add(options, "intensity", 0, 1);
 let step = 0;
-let speed = 0.01;
+// let speed = 0.01;
+const mousePosition = new _three.Vector2();
+window.addEventListener("mousemove", function(e) {
+    mousePosition.x = e.clientX / window.innerWidth * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 - 1;
+});
+const rayCaster = new _three.Raycaster();
 function animate(time) {
     box.rotation.x = time / 1000; //rotate the cube
     box.rotation.y = time / 1000;
     step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step)); //bounce the sphere
+    spotlight.angle = options.angle;
+    spotlight.penumbra = options.penumbra;
+    spotlight.intensity = options.intensity;
+    sLightHelper.update();
+    rayCaster.setFromCamera(mousePosition, camera);
+    const intersects = rayCaster.intersectObjects(scene.children);
+    console.log(intersects);
+    for(let i = 0; i < intersects.length; i++){
+        if (intersects[i].object.id === sphere.id) intersects[i].object.material.color.set(0xFF0000);
+        if (intersects[i].object.name === "theBox") {
+            intersects[i].object.rotation.x = time / 1000; //rotate the cube
+            intersects[i].object.rotation.y = time / 1000;
+        }
+    }
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
 
-},{"three":"ktPTu","three/examples/jsm/controls/OrbitControls.js":"7mqRv","dat.gui":"k3xQk"}],"ktPTu":[function(require,module,exports) {
+},{"three":"ktPTu","three/examples/jsm/controls/OrbitControls.js":"7mqRv","dat.gui":"k3xQk","../img/stars.jpg":"2IYlH","../img/nebula.jpg":"ibF8l","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"ktPTu":[function(require,module,exports) {
 /**
  * @license
  * Copyright 2010-2022 Three.js Authors
@@ -32859,6 +32968,46 @@ var index = {
 };
 exports.default = index;
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["eZyLq","dV6cC"], "dV6cC", "parcelRequire2173")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"2IYlH":[function(require,module,exports) {
+module.exports = require("./helpers/bundle-url").getBundleURL("2MSMO") + "stars.a1d7fe60.jpg" + "?" + Date.now();
+
+},{"./helpers/bundle-url":"lgJ39"}],"lgJ39":[function(require,module,exports) {
+"use strict";
+var bundleURL = {};
+function getBundleURLCached(id) {
+    var value = bundleURL[id];
+    if (!value) {
+        value = getBundleURL();
+        bundleURL[id] = value;
+    }
+    return value;
+}
+function getBundleURL() {
+    try {
+        throw new Error();
+    } catch (err) {
+        var matches = ("" + err.stack).match(/(https?|file|ftp|(chrome|moz|safari-web)-extension):\/\/[^)\n]+/g);
+        if (matches) // The first two stack frames will be this function and getBundleURLCached.
+        // Use the 3rd one, which will be a runtime in the original bundle.
+        return getBaseURL(matches[2]);
+    }
+    return "/";
+}
+function getBaseURL(url) {
+    return ("" + url).replace(/^((?:https?|file|ftp|(chrome|moz|safari-web)-extension):\/\/.+)\/[^/]+$/, "$1") + "/";
+} // TODO: Replace uses with `new URL(url).origin` when ie11 is no longer supported.
+function getOrigin(url) {
+    var matches = ("" + url).match(/(https?|file|ftp|(chrome|moz|safari-web)-extension):\/\/[^/]+/);
+    if (!matches) throw new Error("Origin not found");
+    return matches[0];
+}
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+exports.getOrigin = getOrigin;
+
+},{}],"ibF8l":[function(require,module,exports) {
+module.exports = require("./helpers/bundle-url").getBundleURL("2MSMO") + "nebula.a535bdf2.jpg" + "?" + Date.now();
+
+},{"./helpers/bundle-url":"lgJ39"}]},["eZyLq","dV6cC"], "dV6cC", "parcelRequire2173")
 
 //# sourceMappingURL=index.e82f28a0.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
-import { CubeCamera } from 'three';
+import { AmbientLight, CubeCamera, Raycaster, SpotLightHelper } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import stars from '../img/stars.jpg'
+import nebula from '../img/nebula.jpg'
 
 //Instantiate the renderer
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true;
 
 //Instantiate the scene and camera to be used in render method
 const scene = new THREE.Scene();
@@ -29,13 +32,13 @@ orbit.update();
 
 //add a box in the scene
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xFF00FF });
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xFF00FF });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
 //add a plane in the scene
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: THREE.DoubleSide //so it doesnt dsappear when you go upside down
 });
@@ -43,6 +46,7 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI; //rotate plane inline with the grid
+plane.receiveShadow = true;
 
 // add grid, number controles the number of grids
 const gridHelper = new THREE.GridHelper(30);
@@ -50,12 +54,91 @@ scene.add(gridHelper);
 
 //add a sphere
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
+const sphereMaterial = new THREE.MeshStandardMaterial({
     color: 0x0000FF,
     wireframe: false
 })
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.castShadow = true;
+
+/*
+Ambeint light = light that comes from the environment (indirect result of other sources of light, daylight in your room)
+Directional light = (direct sunlight) big and far source of light that coveres the environment
+spotlight = light in the form of a cone, farther the source of the light the bigger the cone radius
+*/
+
+//Add ambient light 
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+/*
+//Add directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+scene.add(directionalLight);
+directionalLight.position.set( -30, 50, 0)
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.bottom = -12;
+
+//Directional Light helper
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(dLightHelper);
+
+//Shadow Camera
+const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(dLightShadowHelper);
+*/
+
+//Spotlight
+const spotlight = new THREE.SpotLight(0xffffff);
+scene.add(spotlight);
+spotlight.position.set(-100, 100, 0);
+spotlight.castShadow = true;
+spotlight.angle = 0.1;
+
+//Spotlight helper
+const sLightHelper = new THREE.SpotLightHelper(spotlight);
+scene.add(sLightHelper);
+
+//Add fog to the scene
+// scene.fog = new THREE.Fog(0xffffff, 0, 200);
+scene.fog = new THREE.FogExp2(0xffffff, 0.01);
+
+// renderer.setClearColor(0xffea00)
+
+const textureLoader = new THREE.TextureLoader();
+// scene.background = textureLoader.load(nebula);
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    nebula,
+    stars,
+    stars,
+    stars,
+    stars,
+    stars
+]);
+
+const box2MultMaterial = [
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(nebula)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(nebula)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(stars)}),
+
+]
+
+const box2Geometry = new THREE.BoxGeometry(4, 4, 4);
+const box2Material = new THREE.MeshBasicMaterial({
+    // color: 0x00FF00,
+    map: textureLoader.load(nebula)
+});
+const box2 = new THREE.Mesh(box2Geometry, box2MultMaterial);
+scene.add(box2);
+
+box2.position.set(0, 15, 10);
+box2.name = 'theBox';
 
 //reposition sphere
 sphere.position.set(-10, 10, 0);
@@ -65,7 +148,10 @@ const gui = new dat.GUI();
 const options = {
     sphereColor: '#ffea00',
     wireframe: false,
-    speed: 0.01
+    speed: 0.01,
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 1
 };
 
 gui.addColor(options, 'sphereColor').onChange(function(e){
@@ -79,8 +165,21 @@ gui.add(options, 'wireframe').onChange(function(e){
 // add speed with upper and lower bounds
 gui.add(options, 'speed', 0, 0.1);
 
+gui.add(options, 'angle', 0, 1);
+gui.add(options, 'penumbra', 0, 1);
+gui.add(options, 'intensity', 0, 1);
+
+
 let step = 0;
-let speed = 0.01;
+// let speed = 0.01;
+
+const mousePosition = new THREE.Vector2();
+window.addEventListener('mousemove', function(e) {
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 -1;
+})
+
+const rayCaster = new THREE.Raycaster();
 
 function animate(time) {
     box.rotation.x = time / 1000; //rotate the cube
@@ -88,6 +187,26 @@ function animate(time) {
     
     step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step)); //bounce the sphere
+
+    spotlight.angle = options.angle;
+    spotlight.penumbra = options.penumbra;
+    spotlight.intensity = options.intensity;
+    sLightHelper.update();
+
+    rayCaster.setFromCamera(mousePosition, camera);
+    const intersects = rayCaster.intersectObjects(scene.children);
+    console.log(intersects);
+
+    for(let i = 0; i < intersects.length; i++){
+        if(intersects[i].object.id === sphere.id){
+            intersects[i].object.material.color.set(0xFF0000);
+        }
+
+        if(intersects[i].object.name === 'theBox') {
+            intersects[i].object.rotation.x = time / 1000; //rotate the cube
+            intersects[i].object.rotation.y = time / 1000;
+        }
+    }
 
     renderer.render(scene, camera);
 }
